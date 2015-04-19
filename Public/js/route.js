13160b77490c4539dc5adb869642584b2a8357cc
wangjiewen <342916053@qq.com>
2015-04-19 22:18:06 +0800

    add

--- Public/js/route.js
+++ Public/js/route.js
@@ -3,13 +3,67 @@
  */
 
 var Route = {
+    baseUrl:'http://127.0.0.1/travel/',
+    $processguide:$("#processguide"),
+    $items:$("#items"),
+    guideTpl:
+        [
+        '<h3> <%= item.ambiguitySname  %><%= item.maxDay  %>日游  </h3>',
+        '<% for(var i = 0;i<item.arrange.length;i++){ %>',
+        '<a href="#day<%= i %>0"><%= item.arrange[i].curDay %></a>',
+        '<% } %>'
+        ].join(''),
+    routeTpl:[
+        '<% for(var i = 0;i<item.length;i++){ %>',
+            '<% for(var j = 0;j<item[i].list.length;j++){ %>',
+                '<div class="timeitem clearfix" id="day<%= i %><%= j %>">',
+                    '<img class="time_icon" src="<%= url %>/Public/images/timeline_side.png" alt=""><span class="time_title"><%= item[i].curDay %>:<%= item[i].list[j].ambiguitySname %></span>',
+                    '<div class="timeitem_content timeitemControl">',
+                        '<img src="<%= item[i].list[j].fullUrl %>" alt="" class="time_img">',
+                            '<div class="timeitem_descrition">',
+                               '<p>前期制作历经两个月的EDDY自导 自演的轻松爆米花剧终于结束了前期 拍摄.在吸取了第零集的失败经验后,初 次拍摄的小团队在前期拍摄中仍然遇到许多困难，庆幸的是他们取得了长足的发展，片源效果也相当不错。</p>',
+                            '</div>',
+                    '</div>',
+                '</div>',
+            '<% } %>',
+        '<% } %>'
+    ].join(''),
     adjustMapPos: function(){
-        var $main = $("#j-main");
-        var mapLeft = $main.offset().left + $main.width();
-        $("#j-cont-allmap").attr({left:mapLeft});
+        var mapLeft = ($(window).width() - 1020) / 2;
+        $("#j-cont-allmap").css({left:mapLeft,top:0});
+    },
+    getData:function(url){
+        var self = this;
+        var url = this.baseUrl+url;
+        $.ajax({
+            url:url,
+            dataType:"json",
+            type:"POST",
+            success:function(data){
+                self.renderPage(data);
+            }
+
+        });
+    },
+
+    renderPage:function(data){
+        this.$processguide.append(tmpl(this.guideTpl,{item:data}));
+        this.$items.append(tmpl(this.routeTpl,{item:data.arrange,url:this.baseUrl}))
+    },
+    bindEvent:function(){
+
+    },
+    init:function(url,$processguide,$items){
+        this.$processguide=$processguide;
+        this.$items = $items;
+        this.adjustMapPos();
+        this.getData(url);
+        this.bindEvent();
     }
+
 };
 
 $(document).ready(function () {
-    Route.adjustMapPos();
+    var url = 'routes/guangzhou/'+"3_0_96f564316ba8ffd5edcbf6fdd8fc5d3.json";
+    Route.init(url,$("#processguide"),$("#items"));
 });
--- Public/js/util.js
+++ Public/js/util.js
@@ -19,4 +19,19 @@ function replaceTpl(template,data){
         outPrint += temp;  
     }  
     return outPrint;  
-} 
+}
+function tmpl(str , data){
+    var fn = !/\W/.test(str) ? cache[str] = cache[str] ||
+        tmpl(document.getElementById(str).innerHTML) :        // Generate a reusable function that will serve as a template
+
+        new Function("obj", "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+            // Introduce the data as local variables using with(){}
+            "with(obj){p.push('" +
+
+            // Convert the template into pure JavaScript
+            str.replace(/[\r\t\n]/g, " ").split("<%").join("\t").replace(/((^|%>)[^\t]*)'/g, "$1\r").replace(/\t=(.*?)%>/g, "',$1,'").split("\t").join("');").split("%>").join("p.push('").split("\r").join("\\'") +
+            "');}return p.join('');");
+    // Provide some basic currying to the user
+    return data ? fn(data) : fn;
+}
