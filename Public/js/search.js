3b83076a317ec21463c8bd83a4a49d8c8201eab8
yinchuandong <yincd520@sina.com>
2015-04-23 17:14:15 +0800

    增加输入提示

--- Public/js/search.js
+++ Public/js/search.js
@@ -7,19 +7,21 @@ var Search = {
         this.bindEvent();
     },
     bindEvent:function(){
+        var self = this;
         var key = getQueryString("key");
         var day = getQueryString("day");
         var price = getQueryString("price");
         var orderby =  getQueryString("orderby");
 
         $("#condition li").removeClass('active');
+        var $condition = $("#condition");
 
-        $("#condition").find("[day="+day+"]").parent().addClass('active');
-        $("#condition").find("[price="+price+"]").parent().addClass('active');
-        $("#condition").find("[order="+orderby+"]").parent().addClass('active');
+        $condition.find("[day="+day+"]").parent().addClass('active');
+        $condition.find("[price="+price+"]").parent().addClass('active');
+        $condition.find("[order="+orderby+"]").parent().addClass('active');
 
 
-        $("#condition").on("click",".condition-day a",function(){
+        $condition.on("click",".condition-day a",function(){
             var day = $(this).attr("day");
             $("#search-day").val(day);
             $("#search-route").trigger("click");
@@ -38,7 +40,49 @@ var Search = {
         $("#search-route").click(function(){
             $("#search-form").submit();
         });
+
+        $("#search-text").keyup(function (e) {
+            var keyword = $(this).val().trim();
+            self.suggest(keyword);
+        });
+
+        $(document).click(function () {
+            var tipsDlg =  $("#tips-dialog");
+            if(tipsDlg.attr("display") != "none"){
+                tipsDlg.hide();
+            }
+        });
+
+        $("#tips-dialog").on("click","li" ,function(){
+            var val = $(this).text();
+            $("#search-text").val(val);
+        });
+    },
+
+    suggest: function (keyword) {
+        var $tipsDlg = $("#tips-dialog");
+        $.ajax({
+            url: gl_baseUrl + "/index.php/Index/suggest",
+            method:'get',
+            data:{key:keyword},
+            dataType: 'json',
+            success: function (res) {
+                if(res.status == 1){
+                    var data = res.data;
+                    var tpl = replaceTpl('<li class="tips-item">{name}</li>', data);
+                    $tipsDlg.empty();
+                    $tipsDlg.append(tpl);
+                    $tipsDlg.show();
+                }else{
+                    $tipsDlg.empty();
+                    $tipsDlg.hide();
+                }
+
+            }
+
+        });
     }
+
 };
 $(document).ready(function(){
     Search.init();
